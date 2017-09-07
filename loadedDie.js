557239285213652df54cc2f2cf42888261812e3a
Luke Siemens <lukesiemens@gmail.com>
2017-09-07 20:08:54 +0000

    made the loaded die work

---
+++ loadedDie.js
@@ -0,0 +1,24 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+ 
+ var index = 0
+
+  return function() {
+  	
+  	var output = list[index % list.length]
+  	index = index + 1
+  	//if(index > list.length -1){
+  	//	index = 0
+  	//}
+  	return output;
+
+   
+
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
